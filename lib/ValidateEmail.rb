47bbaf93841fd96333b09437c2bca68c255bd082
B V Satyaram <bvsatyaram@gmail.com>
2011-04-27 13:42:21 +0530

    Performance upgrade

--- lib/ValidateEmail.rb
+++ lib/ValidateEmail.rb
@@ -4,7 +4,7 @@ module ValidateEmail
   def self.validate(email, validate_mx = false)
     email_pattern = (email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/)
     is_valid = email_pattern.nil? ? false : true
-    is_valid = is_valid && validate_mx_record(email) if validate_mx
+    is_valid = validate_mx_record(email) if is_valid && validate_mx
 
     return is_valid
   end
